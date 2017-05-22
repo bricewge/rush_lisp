ceddd513a3197e97781e3bb0ec6a0d074433af0b
Brice Waegeneire <brice.wge@gmail.com>
2017-05-22 12:31:58 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+sbcl_url=https://sourceforge.net/projects/sbcl/files/sbcl/1.2.11/sbcl-1.2.11-x86-64-darwin-binary.tar.bz2
+quicklisp_url=http://beta.quicklisp.org/quicklisp.lisp
+export INSTALL_ROOT=/Users/bwaegene/.sbcl
+export SBCL_HOME="$HOME/.sbcl/lib/sbcl"
+export PATH="$HOME/.sbcl/bin":$PATH
+alias sbcl="sbcl --noinform"
+
+# Install SDL
+printf "\e[91mInstall SDL from the MSC\e[0m"
+read
+# Install sblc
+curl -L "${sbcl_url}" | tar xf -
+cd sbcl*
+./install.sh
+cd -
+# Install quicklisp
+printf "\e[91mWhen asked press ENTER and then 7\e[0m"
+read
+curl -LO "${sbcl_url}"
+sbcl --load quicklisp.lisp <<EOF
+(quicklisp-quickstart:install :path
+ (merge-pathnames ".sbcl/quicklisp"
+  (user-homedir-pathname)))
+(ql:add-to-init-file)
+(ql:quickload "lispbuilder-sdl")
+EOF
+rm quicklisp.lisp
+cd ~/.sbcl/quicklisp/dists/quicklisp/software/lispbuilder-*/lispbuilder-sdl/cocoahelper
+make
+cd -
+sbcl <<EOF
+(ql:quickload "lispbuilder-sdl")
+EOF
