ca45f1fbd32aa4ac177d6f0466bcb1b3603bb36f
Brice Waegeneire <brice.wge@gmail.com>
2017-05-23 22:11:56 +0200

    separate environement variable and instalation script

---
+++ env.sh
@@ -0,0 +1,4 @@
+export INSTALL_ROOT=/Users/bwaegene/.sbcl
+export SBCL_HOME="$HOME/.sbcl/lib/sbcl"
+export PATH="$HOME/.sbcl/bin":$PATH
+alias sbcl="sbcl --noinform"
--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
+
+source ./env.zsh
 sbcl_url=https://sourceforge.net/projects/sbcl/files/sbcl/1.2.11/sbcl-1.2.11-x86-64-darwin-binary.tar.bz2
 quicklisp_url=http://beta.quicklisp.org/quicklisp.lisp
-export INSTALL_ROOT=/Users/bwaegene/.sbcl
-export SBCL_HOME="$HOME/.sbcl/lib/sbcl"
-export PATH="$HOME/.sbcl/bin":$PATH
-alias sbcl="sbcl --noinform"
 
 # Install SDL
 printf "\e[91mInstall SDL from the MSC\e[0m"
